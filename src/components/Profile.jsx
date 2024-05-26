a442176d6b5828270d9a06bae3e14682b6b715d7
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-05-26 23:10:44 +0500

    added card profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import image from "../assets/image-1.jpg"
 
 const Profile = () => {
   const navigate = useNavigate();
-  const [userDetails, setUserDetails] = useState();
+  const [userDetails, setUserDetails] = useState("hello");
 
   // useEffect(() => {
   //   const getData = account.get();
@@ -13,7 +14,6 @@ const Profile = () => {
   //   }).catch((err) => console.log(err));
   // }, [])
 
-
   const handleLogout = async () => {
     // try {
     //     await account.deleteSession("current")
@@ -21,13 +21,13 @@ const Profile = () => {
     // } catch (error) {
     //     console.log(error);
     // }
-}
+  };
 
   return (
     <>
       {userDetails ? (
-        <>
-          <div className="min-h-min max-w-7xl mx-auto shadow-md flex justify-between text-right py-3 px-3 mt-2 rounded-md">
+        <div className="flex flex-col justify-center items-center">
+          <div className="min-h-min w-1/2 mx-auto shadow-md flex justify-between text-right py-3 px-3 mt-2 rounded-md">
             <div>
               <p className="text-xl">Hello {userDetails.name}</p>
             </div>
@@ -40,9 +40,52 @@ const Profile = () => {
               </button>
             </div>
           </div>
+
+          <div class="max-w-sm bg-white mt-6 border-gray-200 rounded-lg shadow-xl dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img
+                class="rounded-t-lg"
+                src={image}
+                alt=""
+              />
+            </a>
+            <div class="p-5">
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Noteworthy technology acquisitions 2021
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+              <a
+                href="#"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
           {/* TODO FORM */}
           {/* TODOS BOX */}
-        </>
+        </div>
       ) : (
         <p className="mt-4">
           Please Login To see Profile{" "}
